85a8667344373cbce6ff7612dc2206e6a05b2a08
Juliana Ramalho <124271266+juh-rr@users.noreply.github.com>
2023-02-12 16:14:46 -0300

    Create difficulty_setting.py

---
+++ difficulty_setting.py
@@ -0,0 +1,9 @@
+def difficulty_setting(setting):
+  numTries = 0
+  if setting == "easy":
+    numTries = 10
+    return numTries
+  elif setting == "hard":
+    numTries = 5
+    return numTries
+   
