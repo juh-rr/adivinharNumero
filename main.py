e2d02e06ba72a2e0f79ffc717ca0a6030ee690e0
Juliana Ramalho <124271266+juh-rr@users.noreply.github.com>
2023-02-12 16:14:03 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from art import logo
+from difficulty_setting import difficulty_setting
+import random
+correctAnswer = random.randint(1, 100)
+
+print(logo)
+print("Welcome to the number guessing game!")
+difficulty = input("Choose a difficulty, type 'easy' or 'hard': ").lower()
+
+while difficulty != "easy" and difficulty != "hard":
+  difficulty = input("Please choose a valid difficulty, type 'easy' or 'hard': ").lower()
+
+qtdTries = difficulty_setting(difficulty)
+
+keepTrying = True
+while keepTrying == True:
+  answer = int(input("Please guess a number between 1 and 100: "))
+  if answer == correctAnswer:
+    print(f"Congratulations! You guessed {correctAnswer} right!")
+    keepTrying = False
+  elif answer > correctAnswer:
+     print("Too high!")
+     print(f"You have {qtdTries-1} tries left.")
+     qtdTries -= 1
+     if qtdTries == 0:
+       print(f"You're out of tries! The correct answer was {correctAnswer}.")
+       keepTrying = False
+  elif answer < correctAnswer:
+    print("Too low!")
+    print(f"You have {qtdTries-1} tries left.")
+    qtdTries -= 1
+    if qtdTries == 0:
+      print(f"You're out of tries! The correct answer was {correctAnswer}.")
+      keepTrying = False
